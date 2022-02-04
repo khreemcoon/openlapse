52714e5ed15538e7cf37360b8e43aed50c06296a
khreemcoon <80398208+khreemcoon@users.noreply.github.com>
2022-02-04 14:34:44 +0200

    Update openlapse.sh

--- openlapse.sh
+++ openlapse.sh
@@ -7,8 +7,9 @@ for i in $@
 do
     if [[ $i == "-h" ]]; then
         echo -e "About:\n   OpenLapse is a shell script designed to clone chronolapse on Linux/BSD systems.\n\nChronolapse?\n   Chronolapse is... https://www.chronolapse.com/index.html visit it.\n"
-        echo -e "Usage:\n   openlapse -n FILENAME -f DIRECTORY -d DELAY\n    -h is for help.\n"
+        echo -e "Usage:\n   openlapse -n FILENAME -f DIRECTORY -d DELAY\n    -h is for help.\n    use CTRL+C to exit the app.\n"
         echo -e "Explanation:\n   -n FILENAME: quite self explanatory\n   -f DIRECTORY: quite self explanatory\n   -d DELAY: delay between screenshots\n"
+        echo -e "Send complaints, improvements, suggestions, anything to: dramst8@protonmail.com\n"
         break
     fi
     if [[ $i == "-f" ]]; then
