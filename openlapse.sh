aae9591f662bbbbccdee7fc55e440ae2af09fc6c
khreemcoon <80398208+khreemcoon@users.noreply.github.com>
2022-02-04 14:33:25 +0200

    Add files via upload

---
+++ openlapse.sh
@@ -0,0 +1,36 @@
+DIRECTORY=""
+NAME=""
+DELAY=0
+loopnum=0
+args=("$@")
+for i in $@
+do
+    if [[ $i == "-h" ]]; then
+        echo -e "About:\n   OpenLapse is a shell script designed to clone chronolapse on Linux/BSD systems.\n\nChronolapse?\n   Chronolapse is... https://www.chronolapse.com/index.html visit it.\n"
+        echo -e "Usage:\n   openlapse -n FILENAME -f DIRECTORY -d DELAY\n    -h is for help.\n"
+        echo -e "Explanation:\n   -n FILENAME: quite self explanatory\n   -f DIRECTORY: quite self explanatory\n   -d DELAY: delay between screenshots\n"
+        break
+    fi
+    if [[ $i == "-f" ]]; then
+        DIRECTORY=${args[loopnum+1]}
+    fi
+    if [[ $i == "-n" ]]; then
+        NAME=${args[loopnum+1]}
+    fi
+    if [[ $i == "-d" ]]; then
+        DELAY=${args[loopnum+1]}
+    fi
+    ((++loopnum))
+done
+if [[ "${DIRECTORY: -1}" != "/" && $DIRECTORY != "" ]]; then
+    DIRECTORY+="/"
+fi
+if [[ "$NAME" != *".png"*  || "$NAME" != *".PNG"* ]]; then
+    NAME+=".png"
+fi
+FULL=$DIRECTORY$NAME
+while true
+do
+    sleep $DELAY
+    scrot $FULL
+done
